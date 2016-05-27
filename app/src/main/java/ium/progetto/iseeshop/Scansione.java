349eaa39f212cd4eeabab5c74b178021f27474e7
Roberto <robin994@hotmail.it>
2016-05-27 12:16:36 +0200

    Aggiunta alertDialog se si preme sull'immagine della scansione

--- app/src/main/java/ium/progetto/iseeshop/Scansione.java
+++ app/src/main/java/ium/progetto/iseeshop/Scansione.java
@@ -2,16 +2,44 @@ package ium.progetto.iseeshop;
 
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class Scansione extends FragmentActivity {
-
+    ImageView immagineNfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scansione_layout);
+        immagineNfc = (ImageView) findViewById(R.id.nfc);
+        immagineNfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle(getString(R.string.titleAlert)) //
+                        .setMessage(getString(R.string.textAlert)) //
+                        .setPositiveButton(getString(R.string.showProduct), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // TODO
+                                dialog.dismiss();
+                            }
+                        }) //
+                        .setNegativeButton(getString(R.string.ignoreProduct), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // TODO
+                                dialog.dismiss();
+                            }
+                        });
+                builder.show();
+
+            }
+        });
     }
 }
 
