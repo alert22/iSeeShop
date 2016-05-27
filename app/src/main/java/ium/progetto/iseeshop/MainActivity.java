b066a70eab803eebaeec16d15cf0d82cb7edac1c
Roberto <robin994@hotmail.it>
2016-05-27 21:58:14 +0200

    Aggiunte opzioni alla  toolBar

--- app/src/main/java/ium/progetto/iseeshop/MainActivity.java
+++ app/src/main/java/ium/progetto/iseeshop/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActivityGroup {
 
 
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -78,7 +78,7 @@ public class MainActivity extends ActivityGroup {
     public void showPopup(View v) {
         PopupMenu popup = new PopupMenu(this, v);
         MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.actions, popup.getMenu());
+        inflater.inflate(R.menu.menu_main, popup.getMenu());
         popup.show();
     }
 
